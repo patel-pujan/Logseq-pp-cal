1813afb438429a4b1977f41f92f71500eb5d4ebe
pujan-p <pujanp@uw.edu>
2025-07-26 18:07:35 -0700

    v1

---
+++ src/events.ts
@@ -0,0 +1,74 @@
+import ICAL from 'ical.js';
+import { URL } from "../secret/cals";
+export { parseICS };
+
+interface ClippedEvent {
+    event: ICAL.Event;
+    summary: string;
+    startTime: ICAL.Time;
+}
+
+const period_days = 7
+const startDate = new Date();
+const endDate = new Date();
+endDate.setDate(startDate.getDate() + period_days);
+
+const startTime = ICAL.Time.fromJSDate(startDate, true);
+const endTime = ICAL.Time.fromJSDate(endDate, true);
+console.log("Filtering events between", startTime.toJSDate(), "and", endTime.toJSDate());
+
+async function fetchICS() {
+
+    const res = await fetch(URL);
+    if (!res.ok) throw new Error(`Failed to fetch: ${res.statusText}`);
+
+    const textData = await res.text();
+    const jcalData = ICAL.parse(textData);
+    const comp = new ICAL.Component(jcalData);
+    const vevents = comp.getAllSubcomponents("vevent");
+
+    return vevents;
+}
+
+async function clipEvents(vevents: ICAL.Component[]): Promise<ClippedEvent[]> {
+
+    const eventsThisWeek: ClippedEvent[] = [];
+    vevents.forEach((vevent) => {
+        let event = new ICAL.Event(vevent);
+
+        if (!event.isRecurring() && event.startDate.compare(startTime) >= 0 && event.startDate.compare(endTime) <= 0) {
+            eventsThisWeek.push({
+            event: event,
+            summary: event.summary,
+            startTime: event.startDate.toJSDate.getTime(),
+            });
+            return;
+        }
+
+        let iter = event.iterator();
+        let next;
+        while ((next = iter.next())) {
+            if (next.compare(startTime) >= 0 && next.compare(endTime) <= 0) {
+                let occurrence = event.getOccurrenceDetails(next);
+                eventsThisWeek.push({
+                    event: occurrence.item,
+                    summary: event.summary,
+                    // startTime: event.startDate.toJSDate().getTime(),
+                    startTime: occurrence.startDate,
+                });
+            } else if (next.compare(endTime) > 0) {
+                return;
+            }
+        }
+    });
+
+    return eventsThisWeek;
+}
+
+async function parseICS() {
+    
+    const scheduledEvents = await fetchICS();
+    const relevantEvents = await clipEvents(scheduledEvents);
+
+    return relevantEvents
+}
--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,41 @@
 import '@logseq/libs';
-import ICAL from 'ical.js';
-import { URL } from "../secret/cals";
+import { parseICS } from './events'
 
-const now = new Date();
-const oneWeekLater = new Date(now);
-oneWeekLater.setDate(now.getDate() + 7);
+// const relevantEvents = parseICS();
 
-const defaultStartTime = ICAL.Time.fromJSDate(new Date(0), true);
-const startTime = ICAL.Time.fromJSDate(now, true);
-const endTime = ICAL.Time.fromJSDate(oneWeekLater, true);
-console.log("Filtering events between", startTime.toJSDate(), "and", endTime.toJSDate());
+function main() {
 
-async function fetchAndParseICS() {
-  const res = await fetch(URL);
-  if (!res.ok) throw new Error(`Failed to fetch: ${res.statusText}`);
-  const textData = await res.text(); 
-  const jcalData = ICAL.parse(textData);
-  const comp = new ICAL.Component(jcalData);
-  const vevents = comp.getAllSubcomponents("vevent");
-  
-  const eventsThisWeek = [];
-  vevents.forEach((vevent) => {
-    
-    const event = new ICAL.Event(vevent);
-   
-    if (event.isRecurring()) {
-      const iter = event.iterator();
-      let next;
-
-      while ((next = iter.next())) {
-        if (next.compare(startTime) >= 0 && next.compare(endTime) <= 0) {
-          const occurrence = event.getOccurrenceDetails(next);
-          eventsThisWeek.push({
-            event: occurrence.item,
-            summary: event.summary,
-          });
-        } else if (next.compare(endTime) > 0) {
-          break;
-        }
-      }
-    } else {
-      if (event.startDate.compare(startTime) >= 0 && event.startDate.compare(endTime) <= 0) {
-        eventsThisWeek.push({
-          event: event,
-          summary: event.summary,
-        })
-      ;}
-    }
-  });
-  
-  console.log(eventsThisWeek);
+    logseq.App.registerCommand(
+        'Pull Calendar',
+        {
+            desc: 'pull gcal',
+            key: 'Pull Calendar',
+            label: 'Pull Calendar',
+            palette: true,
+        },
+        populateLogseq
+    );
+    // logseq.Editor.registerSlashCommand(
+    //     'Pull Calendar',
+    //     populateLogseq,
+    // );
 }
 
-logseq.ready(fetchAndParseICS);
+async function populateLogseq() {
+
+    const relevantEvents = await parseICS();
+    const block = await logseq.Editor.getCurrentBlock();
+
+    let newContent = "# Events This Week ";
+
+    for (const event of relevantEvents) {
+        newContent += `\n ${event.summary}`;
+    }
+
+    await logseq.Editor.updateBlock(block.uuid, newContent);
+
+    return;
+}
+
+
+logseq.ready(main).catch(console.error);
